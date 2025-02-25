0a3023e88bde5c6aa12de022324aed5fea651972
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-02-25 09:41:22 +0100

    updating code

---
+++ node/ex00/calculadora.js
@@ -0,0 +1,30 @@
+nomeProgrma = "##########Calculadora Basica##########"
+
+
+function soma(a, b)
+{
+    return a + b
+}
+
+function sub(a, b)
+{
+    return a - b
+}
+
+function mult(a, b)
+{
+    return a * b
+}
+
+function div(a, b)
+{
+    return a / b
+}
+
+module.exports = {
+    soma,
+    sub,
+    mult,
+    div,
+    nomeProgrma
+}
---
+++ node/ex00/index.js
@@ -0,0 +1,7 @@
+var calculadora = require("./calculadora")
+
+console.log(calculadora.nomeProgrma);
+console.log(calculadora.soma(5, 2))
+console.log(calculadora.sub(5, 2))
+console.log(calculadora.mult(5, 2))
+console.log(calculadora.div(5, 2))
