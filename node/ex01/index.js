e96a98a5bfd6c016ddabf5657824d35509eb5832
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-02-25 10:30:31 +0100

    updating code

--- node/ex01/index.js
+++ node/ex01/index.js
@@ -1,1 +1,7 @@
-// teste
+var http = require("http")
+const port = 3000
+
+http.createServer((request, response)=>{
+    response.end("<h1>Seja Bem-vindo!</h1>")
+}).listen(port);
+console.log(`Servidor rodando na porta ${port}`)
