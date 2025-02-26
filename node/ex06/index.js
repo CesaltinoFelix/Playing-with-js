f40796c7d01005129ea1492fb8395d75d84b2907
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-02-26 12:01:34 +0100

    updating code

--- node/ex06/index.js
+++ node/ex06/index.js
@@ -8,9 +8,53 @@ app.get("/", (req, res)=>{
 })
 
 app.get("/home", (req, res)=>{
-    const nome = req.query['nome']
+
+    const products = [
+        {
+            id: 1,
+            name: 'Teclado Mecânico RGB',
+            price: 15000,
+            category: 'Periféricos',
+            inStock: true
+        },
+        {
+            id: 2,
+            name: 'Mouse Gamer',
+            price: 10000,
+            category: 'Periféricos',
+            inStock: false
+        },
+        {
+            id: 3,
+            name: 'Monitor 24" Full HD',
+            price: 60000,
+            category: 'Monitores',
+            inStock: true
+        },
+        {
+            id: 4,
+            name: 'Cadeira Ergonômica',
+            price: 75000,
+            category: 'Móveis',
+            inStock: true
+        },
+        {
+            id: 5, 
+            name: 'Headset Bluetooth',
+            price: 20000,
+            category: 'Áudio',
+            inStock: false
+        }
+    ];
+    
+    const nome = req.query['nome']? req.query['nome'] : 'Desconhecido'
     const email = req.query['email']
-    res.render("index", {nome, email})
+    const password = req.query['password']
+    var showProducts = false;
+    if(password)
+        showProducts = true
+        
+    res.render("index", {nome, email, showProducts, products})
 })
 
 app.listen(3000, (error)=>{
