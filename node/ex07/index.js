788f8a887ac4f73713093f5e5522936aaa4fda37
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-02-26 14:45:07 +0100

    updating code

--- node/ex07/index.js
+++ node/ex07/index.js
@@ -1,10 +1,13 @@
 const { render } = require("ejs")
 const express = require("express")
+const bodyParser = require("body-parser");
+const sequelize = require("./database/database");
 const app = express()
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get("/", (req, res)=>{
     res.render("index")
@@ -12,6 +15,10 @@ app.get("/", (req, res)=>{
 
 app.get("/adicionar-pergunta", (req, res)=>{
     res.render("adicionar_pergunta")
+})
+
+app.post("/adicionar-pergunta/create", (req, res)=>{
+    res.send("pergunta adicionada")
 })
 
 app.listen(3000, (error)=>{
