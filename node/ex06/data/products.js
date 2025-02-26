7ff952cc14053753569bff7f542208478f9cec88
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-02-26 12:14:08 +0100

    updating code

---
+++ node/ex06/data/products.js
@@ -0,0 +1,39 @@
+const products = [
+    {
+        id: 1,
+        name: 'Teclado Mecânico RGB',
+        price: 15000,
+        category: 'Periféricos',
+        inStock: true
+    },
+    {
+        id: 2,
+        name: 'Mouse Gamer',
+        price: 10000,
+        category: 'Periféricos',
+        inStock: false
+    },
+    {
+        id: 3,
+        name: 'Monitor 24" Full HD',
+        price: 60000,
+        category: 'Monitores',
+        inStock: true
+    },
+    {
+        id: 4,
+        name: 'Cadeira Ergonômica',
+        price: 75000,
+        category: 'Móveis',
+        inStock: true
+    },
+    {
+        id: 5, 
+        name: 'Headset Bluetooth',
+        price: 20000,
+        category: 'Áudio',
+        inStock: false
+    }
+];
+
+module.exports = products
--- node/ex06/index.js
+++ node/ex06/index.js
@@ -1,51 +1,15 @@
 const express = require("express")
+const products = require("./data/products")
 const app = express()
 
 app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.get("/", (req, res)=>{
     res.render("login")
 })
 
 app.get("/home", (req, res)=>{
-
-    const products = [
-        {
-            id: 1,
-            name: 'Teclado Mecânico RGB',
-            price: 15000,
-            category: 'Periféricos',
-            inStock: true
-        },
-        {
-            id: 2,
-            name: 'Mouse Gamer',
-            price: 10000,
-            category: 'Periféricos',
-            inStock: false
-        },
-        {
-            id: 3,
-            name: 'Monitor 24" Full HD',
-            price: 60000,
-            category: 'Monitores',
-            inStock: true
-        },
-        {
-            id: 4,
-            name: 'Cadeira Ergonômica',
-            price: 75000,
-            category: 'Móveis',
-            inStock: true
-        },
-        {
-            id: 5, 
-            name: 'Headset Bluetooth',
-            price: 20000,
-            category: 'Áudio',
-            inStock: false
-        }
-    ];
     
     const nome = req.query['nome']? req.query['nome'] : 'Desconhecido'
     const email = req.query['email']
